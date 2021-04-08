53cfa2fc7373677be17f03961cda31db61b930d4
Albert Wolchesky <wolchesky@google.com>
2021-04-08 13:36:08 -0400

    update versions

--- terraform/security/provider.tf
+++ terraform/security/provider.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "3.52.0"
+      version = "3.63.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
