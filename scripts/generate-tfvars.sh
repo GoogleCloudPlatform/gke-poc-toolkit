10cf0a625d88af77bea5536ab145ebec420eb950
Albert Wolchesky <albert.wolchesky@outlook.com>
2021-08-19 01:22:34 -0400

    commit

--- scripts/generate-tfvars.sh
+++ scripts/generate-tfvars.sh
@@ -72,7 +72,7 @@ shared_vpc_ip_range_pods_name     = "${POD_IP_RANGE_NAME}"
 shared_vpc_ip_range_services_name = "${SERVICE_IP_RANGE_NAME}"
 cluster_config					  = {
     "${CLUSTER_TYPE}" = {
-        region:           = ${REGION}
+        region:           = "${REGION}"
         subnet_name:      = ${CLUSTER_TYPE}-cluster-subnet
     }
 }
