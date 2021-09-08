7089cf558b5f5c4715d8b4baefcc6d414d065c30
Albert Wolchesky <albert.wolchesky@outlook.com>
2021-09-07 21:24:57 -0400

    Update provider.tf

--- terraform/cluster_build/provider.tf
+++ terraform/cluster_build/provider.tf
@@ -38,7 +38,7 @@ provider "kubernetes" {
   host                   = "https://${module.gke.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
-}
+} 
 
 provider "google" {
   project = var.project_id
