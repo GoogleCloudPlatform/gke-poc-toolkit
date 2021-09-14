d53fb8a05ed516bdf66486e539d508152fba22d2
Albert Wolchesky <albert.wolchesky@outlook.com>
2021-09-13 21:34:05 -0400

    Update outputs.tf

--- terraform/cluster_build/outputs.tf
+++ terraform/cluster_build/outputs.tf
@@ -51,3 +51,7 @@
 #   value       = (var.private_endpoint ? "HTTPS_PROXY=localhost:8888 kubectl get pods --all-namespaces" : "kubectl get pods --all-namespaces")
 
 # }
+
+output "nested_secondary_subnets" {
+  value       = local.nested_secondary_subnets
+}
