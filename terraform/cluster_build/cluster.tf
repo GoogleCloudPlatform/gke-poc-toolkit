62c79cebf414a33e50a55f2567fe418cce882ffc
Albert Wolchesky <albert.wolchesky@outlook.com>
2021-09-14 02:06:05 -0400

    Update cluster.tf

--- terraform/cluster_build/cluster.tf
+++ terraform/cluster_build/cluster.tf
@@ -14,64 +14,64 @@
  * limitations under the License.
  */
 
-# module "gke" {
-#   depends_on = [
-#     module.bastion,
-#     module.kms,
-#   ]
-#   for_each                = var.cluster_config
-#   source                  = "terraform-google-modules/kubernetes-engine/google//modules/safer-cluster"
-#   version                 = "14.0.1"
-#   project_id              = module.enabled_google_apis.project_id
-#   name                    = "${each.key}-endpoint-cluster"
-#   region                  = each.value.region
-#   config_connector        = var.config_connector
-#   network                 = local.network_name
-#   subnetwork              = each.value.subnet_name
-#   network_project_id      = local.project_id
-#   ip_range_pods           = local.ip_range_pods
-#   ip_range_services       = local.ip_range_services
-#   enable_private_endpoint = var.private_endpoint
-#   enable_shielded_nodes   = true
-#   master_ipv4_cidr_block  = "172.16.0.16/28"
-#   master_authorized_networks = [{
-#     cidr_block   = var.private_endpoint ? "${module.bastion[0].ip_address}/32" : "${var.auth_ip}/32"
-#     display_name = var.private_endpoint ? "Bastion Host" : "Workstation Public IP"
-#   }]
+module "gke" {
+  depends_on = [
+    module.bastion,
+    module.kms,
+  ]
+  for_each                = var.cluster_config
+  source                  = "terraform-google-modules/kubernetes-engine/google//modules/safer-cluster"
+  version                 = "14.0.1"
+  project_id              = module.enabled_google_apis.project_id
+  name                    = "${each.key}-endpoint-cluster"
+  region                  = each.value.region
+  config_connector        = var.config_connector
+  network                 = local.network_name
+  subnetwork              = each.value.subnet_name
+  network_project_id      = local.project_id
+  ip_range_pods           = local.ip_range_pods
+  ip_range_services       = local.ip_range_services
+  enable_private_endpoint = var.private_endpoint
+  enable_shielded_nodes   = true
+  master_ipv4_cidr_block  = "172.16.0.16/28"
+  master_authorized_networks = [{
+    cidr_block   = var.private_endpoint ? "${module.bastion[0].ip_address}/32" : "${var.auth_ip}/32"
+    display_name = var.private_endpoint ? "Bastion Host" : "Workstation Public IP"
+  }]
 
-#   compute_engine_service_account = local.gke_service_account_email
-#   database_encryption = [{
-#     state    = "ENCRYPTED"
-#     key_name = local.database-encryption-key
-#   }]
+  compute_engine_service_account = local.gke_service_account_email
+  database_encryption = [{
+    state    = "ENCRYPTED"
+    key_name = local.database-encryption-key
+  }]
 
-#   node_pools = local.cluster_node_pools
+  node_pools = local.cluster_node_pools
 
-#   node_pools_oauth_scopes = {
-#     all = []
-#     (var.node_pool) = [
-#       "https://www.googleapis.com/auth/cloud-platform",
-#     ]
-#   }
-#   node_pools_labels = {
-#     all = {}
+  node_pools_oauth_scopes = {
+    all = []
+    (var.node_pool) = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+  node_pools_labels = {
+    all = {}
 
-#     default-node-pool = {
-#       default-node-pool = false
-#     }
-#   }
+    default-node-pool = {
+      default-node-pool = false
+    }
+  }
 
-#   node_pools_metadata = {
-#     all = {}
+  node_pools_metadata = {
+    all = {}
 
-#     (var.node_pool) = {
-#       // Set metadata on the VM to supply more entropy
-#       google-compute-enable-virtio-rng = "true"
-#       // Explicitly remove GCE legacy metadata API endpoint
-#       disable-legacy-endpoints = "true"
-#     }
-#   }
-# }
+    (var.node_pool) = {
+      // Set metadata on the VM to supply more entropy
+      google-compute-enable-virtio-rng = "true"
+      // Explicitly remove GCE legacy metadata API endpoint
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
 
 // Bind the KCC operator Kubernetes service account(KSA) to the 
 // KCC Google Service account(GSA) so the KSA can assume the workload identity users role.
