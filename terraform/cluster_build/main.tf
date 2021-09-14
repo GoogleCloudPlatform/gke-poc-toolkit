122200adec8be8213aa58d5af414478801625959
Albert Wolchesky <albert.wolchesky@outlook.com>
2021-09-14 12:40:07 -0400

    test w/o cluster

--- terraform/cluster_build/main.tf
+++ terraform/cluster_build/main.tf
@@ -31,8 +31,10 @@ locals {
   vpc_selflink              = format("projects/%s/global/networks/%s", local.project_id, local.network_name)
   ip_range_pods             = var.shared_vpc ? var.shared_vpc_ip_range_pods_name : var.ip_range_pods_name
   ip_range_services         = var.shared_vpc ? var.shared_vpc_ip_range_services_name : var.ip_range_services_name
-  regions_with_clusters     = flatten([
-    for cluster in var.cluster_config : ["${cluster.region}"]
+  regions_with_clusters     = distinct([ 
+    flatten([
+      for cluster in var.cluster_config : ["${cluster.region}"]
+    ])
   ])
 
   // Presets for KMS and Key Ring
