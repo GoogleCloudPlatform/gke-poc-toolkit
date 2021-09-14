3029234f0ec95f59b0113bd3048da000669c0196
Albert Wolchesky <albert.wolchesky@outlook.com>
2021-09-14 00:36:38 -0400

    Update main.tf

--- terraform/cluster_build/main.tf
+++ terraform/cluster_build/main.tf
@@ -55,7 +55,7 @@ locals {
     for cluster in var.cluster_config : [
       {
         subnet_name           = cluster.subnet_name
-        subnet_ip             = index(keys(var.cluster_config), cluster)
+        subnet_ip             = index(keys(var.cluster_config), cluster.key)
         subnet_region         = cluster.region
         subnet_private_access = true
         description           = "This subnet is managed by Terraform"
