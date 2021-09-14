0c8b8ad7d8b990a238ab311c0c5a411f5aca9007
Albert Wolchesky <albert.wolchesky@outlook.com>
2021-09-14 17:40:42 -0400

    Update generate-tfvars.sh

--- scripts/generate-tfvars.sh
+++ scripts/generate-tfvars.sh
@@ -55,7 +55,7 @@ cat <<-EOF > "${TFVARS_FILE}"
 project_id                        = "${PROJECT}"
 governance_project_id             = "${GOVERNANCE_PROJECT}"
 shared_vpc                        = true
-shared_vpc_name                   = gke-tk-shared-vpc
+shared_vpc_name                   = "gke-tk-shared-vpc"
 shared_vpc_project_id             = "alw-tk-svpc-10"
 shared_vpc_ip_range_pods_name     = "ip-range-pods"
 shared_vpc_ip_range_services_name = "ip-range-svc"
