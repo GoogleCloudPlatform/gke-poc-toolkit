b3955dc4fd385c619d25c31e58f30c8f3f4d03b1
Albert Wolchesky <albert.wolchesky@outlook.com>
2021-09-30 19:14:51 -0400

    Update cluster.tf

--- terraform/cluster_build/cluster.tf
+++ terraform/cluster_build/cluster.tf
@@ -47,12 +47,12 @@ module "gke" {
 
   node_pools = local.cluster_node_pools
 
-  node_pools_oauth_scopes = {
-    all = []
-    (var.node_pool) = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-  }
+  # node_pools_oauth_scopes = {
+  #   all = []
+  #   (var.node_pool) = [
+  #     "https://www.googleapis.com/auth/cloud-platform",
+  #   ]
+  # }
   node_pools_labels = {
     # all = {} default set in terraform-google-kubernetes-engine
 
