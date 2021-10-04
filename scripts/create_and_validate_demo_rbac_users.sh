2d681f5f09708ee93dc525e6e947edc45deb7f6a
Albert Wolchesky <albert.wolchesky@outlook.com>
2021-10-04 18:19:27 -0400

    modify script to add checks

--- scripts/create_and_validate_demo_rbac_users.sh
+++ scripts/create_and_validate_demo_rbac_users.sh
@@ -33,8 +33,8 @@ declare -a k8s_users=(
 for cluster in ${GKE_CLUSTERS}
 do
     CREDENTIALS="$(terraform output --state=terraform/cluster_build/terraform.tfstate get_credential_commands | grep $cluster | cut -d'"' -f 2 | tr -d \")"
-    tput setaf 1; echo "Creating sample cluster role bindings for cluster: $cluster" ; tput sgr0
-    echo $CREDENTIALS 
+    tput setaf 3; echo "Creating sample cluster role bindings for cluster: $cluster" ; tput sgr0
+    echo "Cluster credential command used to authenticate to cluster: $CREDENTIALS"
     $CREDENTIALS
 
     # Inner Loop - Create Cluster Role Bindings for demo k8s_users
@@ -57,13 +57,49 @@ roleRef:
   name: $ROLE_PERM
   apiGroup: rbac.authorization.k8s.io
 EOF
+
+        # Validate rbac permissions work by validating access to cluster secrets
+        gcloud auth activate-service-account --key-file ./creds/$ROLE_NAME@$PROJECT_ID.iam.gserviceaccount.com.json
+
         # Check if using internal-ip - If yes: proxy kubectl command through proxy, if no: don't 
         if [[ $CREDENTIALS == *"internal-ip"* ]]; then
+
+            # Check for proxy connection if internal IP - Create if not detected 
+            echo "Private cluster detected. Confirming SSH Bastion Tunnel/Proxy"
+            if [[ ! "$(pgrep -f L8888:127.0.0.1:8888)" ]]; then
+              echo "Did not detect a running SSH tunnel.  Opening a new one."
+              BASTION_CMD="$(terraform output --state=terraform/cluster_build/terraform.tfstate bastion_ssh_command | tr -d \")"
+              $BASTION_CMD -T -f tail -f /dev/null
+            else
+              echo "Detected a running SSH tunnel.  Skipping."
+            fi
+
+            # Create the cluster role binding
             HTTPS_PROXY=localhost:8888 kubectl apply -f new_role.yaml
+
+            # Authenticate as sample RBAC user and check for access to cluster secrets
+            $CREDENTIALS
+            CAN_ACCCESS_SECRET="$(HTTPS_PROXY=localhost:8888 kubectl auth can-i get secrets)"
+
         else 
+            # Create the cluster role binding
             kubectl apply -f new_role.yaml
+
+            # Authenticate as sample RBAC user and check for access to cluster secrets
+            $CREDENTIALS
+            CAN_ACCCESS_SECRET="$(kubectl auth can-i get secrets)"
         fi
         
+        # Output secret check result      
+        if [[ $CAN_ACCESS_SECRET == *"yes"* ]]; then
+          tput setaf 3; echo "Service Account: $ROLE_NAME@$PROJECT_ID.iam.gserviceaccount.com can access kubernetes secrets for cluster: $cluster" ; tput sgr0
+        else
+          tput setaf 3; echo "Service Account: $ROLE_NAME@$PROJECT_ID.iam.gserviceaccount.com can NOT access kubernetes secrets for cluster: $cluster" ; tput sgr0
+        fi
+
+        # Revoke service account auth and return to default session auth
+        gcloud auth revoke $ROLE_NAME@$PROJECT_ID.iam.gserviceaccount.com
+
     # End Inner Loop
     done
 # End Outer Loop
