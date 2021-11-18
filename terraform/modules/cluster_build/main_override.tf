ab25c7c76d1e5f1bce9277bde1bd42033b9de53c
Nicholas Eberts <nickeberts@google.com>
2021-11-18 09:29:23 -0500

    test

--- terraform/modules/cluster_build/main_override.tf
+++ terraform/modules/cluster_build/main_override.tf
@@ -1,3 +1,11 @@
 module "acm" {
+  depends_on = [
+    module.gke,
+  ]
+  count             = var.config_sync ? 1 : 0
   source            = "github.com/knee-berts/gke-poc-toolkit//terraform/modules/"
+  project_id        = module.enabled_google_apis.project_id
+  policy_controller = var.policy_controller
+  cluster_config    = var.cluster_config
+  email             = data.google_client_openid_userinfo.me.email
 }
