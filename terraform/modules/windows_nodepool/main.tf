a8ff3ec09ece882fa0b376ecaf99a081a85d0470
Albert Wolchesky <albert.wolchesky@outlook.com>
2021-11-18 15:17:37 -0500

    update

--- terraform/modules/windows_nodepool/main.tf
+++ terraform/modules/windows_nodepool/main.tf
@@ -1,5 +1,5 @@
 // Defines vars so that we can pass them in from cluster_build/cluster.tf from the overall tfvars
-variable "cluster_ids" {
+variable "clusters" {
 }
 
 variable "name" {
@@ -39,7 +39,7 @@ variable "enable_secure_boot" {
 // Add Windows Node Pool to each GKE cluster created
 // https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_node_pool 
 resource "google_container_node_pool" "primary" {
-  for_each           = cluster_ids
+  for_each           = flatten([for each_cluster in var.clusters : each_cluster.cluster_id]
   name               = var.name
   cluster            = each.value
   initial_node_count = var.initial_node_count
