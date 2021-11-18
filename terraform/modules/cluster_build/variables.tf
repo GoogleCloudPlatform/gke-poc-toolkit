2f969b8032eef1f81138396dacddc2eb1f6833fe
Nicholas Eberts <nickeberts@google.com>
2021-11-18 08:19:57 -0500

    testing acm module var

--- terraform/modules/cluster_build/variables.tf
+++ terraform/modules/cluster_build/variables.tf
@@ -174,6 +174,7 @@ variable "k8s_users" {
 }
 
 variable "acm_tf_module_repo" {
+  type = string
   description = "Repo used "
   default = "github.com/GoogleCloudPlatform/gke-poc-toolkit//terraform/modules/acm"
 }
