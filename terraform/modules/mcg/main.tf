21775673e6c8407bfbbea0cc1dca89dc8bf7295e
Nicholas Eberts <nickeberts@google.com>
2021-12-02 12:08:55 -0500

    update

--- terraform/modules/mcg/main.tf
+++ terraform/modules/mcg/main.tf
@@ -43,7 +43,7 @@ module "enabled_google_apis" {
 module "firewall_rules" {
   source       = "terraform-google-modules/network/google//modules/firewall-rules"
   project_id   = var.shared_vpc_project_id
-  network_name = var.vpc_name
+  network_name = var.shared_vpc ? var.shared_vpc_name : var.vpc_name
 
   rules = [{
     name                    = "allow-glcb-backend-ingress"
