96e1dafa5a5eafdf29f031654a06537411173c03
Nicholas Eberts <nickeberts@google.com>
2021-12-02 18:44:02 -0500

    up

--- cli/pkg/cli_init/anthos/main.tf
+++ cli/pkg/cli_init/anthos/main.tf
@@ -7,7 +7,6 @@ module "cluster_build" {
   shared_vpc            = var.shared_vpc
   shared_vpc_project_id = var.shared_vpc_project_id
   network_name          = var.shared_vpc ? var.shared_vpc_name : var.vpc_name
-  shared_vpc_name       = var.shared_vpc_name
 }
 
 variable "project_id" {
