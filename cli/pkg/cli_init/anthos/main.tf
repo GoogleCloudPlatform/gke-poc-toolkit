1a5b869a066ed7e09c98aa8d85d6fa1b8d3c1746
Nicholas Eberts <nickeberts@google.com>
2021-12-02 19:08:12 -0500

    git

--- cli/pkg/cli_init/anthos/main.tf
+++ cli/pkg/cli_init/anthos/main.tf
@@ -6,7 +6,7 @@ module "cluster_build" {
   email                 = data.google_client_openid_userinfo.me.email
   shared_vpc            = var.shared_vpc
   shared_vpc_project_id = var.shared_vpc_project_id
-  network_name          = var.shared_vpc ? var.shared_vpc_name : var.vpc_name
+  network_name          = var.network_name
 }
 
 variable "project_id" {
@@ -21,7 +21,5 @@ variable "shared_vpc_project_id" {
 }
 variable "shared_vpc" {
 }
-variable "vpc_name" {
-}
-variable "shared_vpc_name" {
+variable "network_name" {
 }
