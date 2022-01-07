293e0a8e36cc654f9b286382b2a16052ff74362f
Nicholas Eberts <nickeberts@google.com>
2022-01-07 16:24:31 -0500

    test

--- terraform/modules/mcg/scripts/install_crds.sh
+++ terraform/modules/mcg/scripts/install_crds.sh
@@ -22,4 +22,4 @@ kubectl wait --for=condition=established crd customresourcedefinition.apiextensi
 kubectl wait --for=condition=established crd customresourcedefinition.apiextensions.k8s.io/tlsroutes.networking.x-k8s.io --timeout=10m
 kubectl wait --for=condition=established crd customresourcedefinition.apiextensions.k8s.io/udproutes.networking.x-k8s.io --timeout=10m
 
-rm ./kubeconfig
+# rm ./kubeconfig
