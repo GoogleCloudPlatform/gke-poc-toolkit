4408a51f12f0751f82f2170d3872fcf816deb682
Nicholas Eberts <nickeberts@google.com>
2022-01-24 08:42:51 -0500

    update

--- terraform/modules/mcg/mcg.tf
+++ terraform/modules/mcg/mcg.tf
@@ -69,20 +69,20 @@ resource "google_gke_hub_feature" "mcs" {
 }
 
 // enable Multi-cluster Ingress(also gateway) project wide
-resource "google_gke_hub_feature" "mci" {
-  depends_on = [
-    null_resource.exec_gke_mesh,
-  ]
-  name = "multiclusteringress"
-  location = "global"
-  project  = var.project_id
-  spec {
-    multiclusteringress {
-      config_membership = "projects/${var.project_id}/locations/global/memberships/${keys(var.cluster_config)[0]}-membership"
-    }
-  }
-  provider = google-beta
-}
+# resource "google_gke_hub_feature" "mci" {
+#   depends_on = [
+#     null_resource.exec_gke_mesh,
+#   ]
+#   name = "multiclusteringress"
+#   location = "global"
+#   project  = var.project_id
+#   spec {
+#     multiclusteringress {
+#       config_membership = "projects/${var.project_id}/locations/global/memberships/${keys(var.cluster_config)[0]}-membership"
+#     }
+#   }
+#   provider = google-beta
+# }
 
 // Create IAM binding allowing the hub project's GKE Hub service account access to the registered member project
 resource "google_project_iam_binding" "gkehub-serviceagent" {
