023c893413a8c596922b3e63574baa9afb7b22f1
Nicholas Eberts <nickeberts@google.com>
2022-02-08 12:47:44 -0500

    up

--- terraform/modules/asm/scripts/install_mesh.sh
+++ terraform/modules/asm/scripts/install_mesh.sh
@@ -16,9 +16,6 @@ gcloud beta container hub mesh enable --project=${PROJECT_ID}
 # Verify CRD is established in the cluster
 kubectl wait --for=condition=established crd controlplanerevisions.mesh.cloud.google.com --timeout=10m
 
-# Write ASM control plane revision out to files
-echo ${ASM_RELEASE_CHANNEL} > ./manifests/asm-control-plane-revision.yaml
-
 # Install SAs, Roles, Roledbinding for ASM
 kubectl apply -f ./manifests/istio-system-ns.yaml
 kubectl apply -f ./manifests/
