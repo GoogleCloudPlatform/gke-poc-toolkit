c905f9f4d8148618eee05bc00c6190960e7fa74a
Nicholas Eberts <nickeberts@google.com>
2022-02-09 15:37:06 -0500

    up

--- terraform/modules/asm/scripts/install_mesh.sh
+++ terraform/modules/asm/scripts/install_mesh.sh
@@ -15,8 +15,18 @@ gcloud container clusters get-credentials ${CLUSTER} --region ${LOCATION} --proj
 echo -e "Enabling ASM Mesh on the GKE HUB"
 gcloud beta container hub mesh enable --project=${PROJECT_ID}
 
+NUM_MEMBERS=`gcloud beta container hub memberships list --project=${PROJECT_ID} --format="value(name)" | wc -l | awk '{print $1}'`
+STATE_CODE="OK"
+for i in {1..$NUM_MEMBERS}; do
+    if [[ $i -eq 1 ]]; then
+        echo ${STATE_CODE}
+    else
+        STATE_CODE="${STATE_CODE};OK"
+    fi
+done 
+
 for i in {1..600}; do
-    if [[ `gcloud beta container hub mesh describe --project=${PROJECT_ID} --format="value(membershipStates[].state.code)"` == "OK;OK" ]]; then
+    if [[ `gcloud beta container hub mesh describe --project=${PROJECT_ID} --format="value(membershipStates[].state.code)"` == ${STATE_CODE} ]]; then
         break
     else
         echo "Waiting on hub to install ASM in clusters for $i seconds."
