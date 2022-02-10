30b0dfc3fc3b09bfc2ec853b7a799a266a65fc42
Nicholas Eberts <nickeberts@google.com>
2022-02-10 12:07:57 -0500

    up

--- terraform/modules/asm/scripts/create_kube_config.sh
+++ terraform/modules/asm/scripts/create_kube_config.sh
@@ -5,6 +5,14 @@ echo -e "Project is ${PROJECT_ID}"
 echo -e "CLUSTER is ${CLUSTER}"
 echo -e "LOCATION is ${LOCATION}"
 
+# Create kubeconfig and get cluster creds
+echo -e "Adding cluster ${CLUSTER} to kubeconfig located at ${PWD}/tempkubeconfig"
+if [[ ! -e ./tempkubeconfig ]]; then
+    touch ./tempkubeconfig
+fi
+export KUBECONFIG=./tempkubeconfig
+gcloud container clusters get-credentials ${CLUSTER} --region ${LOCATION} --project ${PROJECT_ID}
+
 # Download ASM installation package for istioctl bin
 if [[ ${OSTYPE} == 'darwin'* ]]; then
     export ASM_PACKAGE_OS="${ASM_PACKAGE}-osx.tar.gz"
@@ -13,11 +21,3 @@ else
 fi
 curl -LO https://storage.googleapis.com/gke-release/asm/"${ASM_PACKAGE_OS}"
 tar xzf ${ASM_PACKAGE_OS} && rm -rf ${ASM_PACKAGE_OS}
-
-# Create kubeconfig and get cluster creds
-echo -e "Adding cluster ${CLUSTER} to kubeconfig located at ${PWD}/tempkubeconfig"
-if [[ ! -e ./tempkubeconfig ]]; then
-    touch ./tempkubeconfig
-fi
-export KUBECONFIG=./tempkubeconfig
-gcloud container clusters get-credentials ${CLUSTER} --region ${LOCATION} --project ${PROJECT_ID}
