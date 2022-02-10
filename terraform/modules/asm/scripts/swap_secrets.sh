c148efec80d6e28c1dabc3d805c24e5b9778a56a
Nicholas Eberts <nickeberts@google.com>
2022-02-10 10:50:37 -0500

    up

--- terraform/modules/asm/scripts/swap_secrets.sh
+++ terraform/modules/asm/scripts/swap_secrets.sh
@@ -13,16 +13,16 @@ echo -e "TARGET_LOCATION is ${TARGET_LOCATION}"
 ISTIOCTL_CMD=./${ASM_PACKAGE}/bin/istioctl
 
 # Setup kubeconfig
-echo -e "Setting up kubeconfig at ${MODULE_PATH}/tempkubeconfig"
+echo -e "Setting up kubeconfig at ./tempkubeconfig"
 export KUBECONFIG=./tempkubeconfig
 
 echo -e "KUBECONFIG set to: ${KUBECONFIG}"
 
 # Create kubeconfig secret for the current cluster and install it in istio-system of the rest of the mesh clusters
-if [[ ${CLUSTER} == ${TARGET_CLUSTER} ]]; then
-    break
-else
+if [[ ${CLUSTER} != ${TARGET_CLUSTER} ]]; then
     echo -e "Creating kubeconfig secret from cluster ${CLUSTER} and installing it on cluster ${TARGET_CLUSTER}"
     ${ISTIOCTL_CMD} x create-remote-secret --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER} --name=${CLUSTER} > ./manifests/secret-kubeconfig-${CLUSTER}.yaml
     kubectl apply -f ./manifests/secret-kubeconfig-${CLUSTER}.yaml --kubeconfig=${KUBECONFIG} --context=gke_${PROJECT_ID}_${TARGET_LOCATION}_${TARGET_CLUSTER}
+else
+    echo -e "Skipping as the current cluster, ${CLUSTER}, is the same as the target cluster, ${TARGET_CLUSTER}"
 fi
