e6aabe944c21da8b60472cbfa756c539b3f56065
Nicholas Eberts <nickeberts@google.com>
2022-02-09 20:42:55 -0500

    up

--- terraform/modules/asm/scripts/install_mesh.sh
+++ terraform/modules/asm/scripts/install_mesh.sh
@@ -14,23 +14,23 @@ export KUBECONFIG=./tempkubeconfig
 echo -e "Enabling ASM Mesh on the GKE HUB"
 gcloud beta container hub mesh enable --project=${PROJECT_ID}
 
-# NUM_MEMBERS=`gcloud beta container hub memberships list --project=${PROJECT_ID} --format="value(name)" | wc -l | awk '{print $1}'`
-# STATE_CODE="OK"
-# for i in {1..$NUM_MEMBERS}; do
-#     if [[ $i -eq 1 ]]; then
-#         echo ${STATE_CODE}
-#     else
-#         STATE_CODE="${STATE_CODE};OK"
-#     fi
-# done 
+NUM_MEMBERS=`gcloud beta container hub memberships list --project=${PROJECT_ID} --format="value(name)" | wc -l | awk '{print $1}'`
+STATE_CODE="OK"
+for i in {1..$NUM_MEMBERS}; do
+    if [[ $i -eq 1 ]]; then
+        echo ${STATE_CODE}
+    else
+        STATE_CODE="${STATE_CODE};OK"
+    fi
+done 
 
-# for i in {1..600}; do
-#     if [[ `gcloud beta container hub mesh describe --project=${PROJECT_ID} --format="value(membershipStates[].state.code)"` == ${STATE_CODE} ]]; then
-#         break
-#     else
-#         echo "Waiting on hub to install ASM in clusters for $i seconds."
-#     fi
-# done
+for i in {1..600}; do
+    if [[ `gcloud beta container hub mesh describe --project=${PROJECT_ID} --format="value(membershipStates[].state.code)"` == ${STATE_CODE} ]]; then
+        break
+    else
+        echo "Waiting on hub to install ASM in clusters for $i seconds."
+    fi
+done
 
 # Verify CRD is established in the cluster
 echo -e "Verifying Control Plane Revisions CRD is present on ${CLUSTER}"
