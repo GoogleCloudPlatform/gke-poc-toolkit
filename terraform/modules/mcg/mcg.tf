c1e95f70f550ee676e2b9c6224b01a518ab93c17
Nicholas Eberts <nickeberts@google.com>
2022-02-09 23:26:24 -0500

    up

--- terraform/modules/mcg/mcg.tf
+++ terraform/modules/mcg/mcg.tf
@@ -1,133 +1,112 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Defines vars so that we can pass them in from cluster_build/main.tf from the overall tfvars
-variable "project_id" {
+variable "cluster_name" {}
+variable "cluster_config" {}
+variable "vpc_name" {}
+variable "vpc_project_id" {}
+variable "project_id" {}
+variable "location" {}
+variable "asm_version" {}
+variable "asm_release_channel" {}
+variable "asm_package" {}
+
+locals {
+  cluster_count = length(var.cluster_config)
 }
 
-variable "vpc_project_id" {
-}
-variable "cluster_config" {
-}
-variable "vpc_name" {
-}
-variable "gateway_crds_version" {
-  default = "v0.3.0"     
-}
-
-data "google_project" "project" {
-  project_id = var.project_id
-}
-
-// https://cloud.google.com/kubernetes-engine/docs/how-to/multi-cluster-ingress-setup#shared_vpc_deployment 
-module "firewall_rules" {
-  source       = "terraform-google-modules/network/google//modules/firewall-rules"
-  project_id   = var.vpc_project_id
-  network_name = var.vpc_name
-
-  rules = [{
-    name                    = "allow-glcb-backend-ingress"
-    description             = null
-    direction               = "INGRESS"
-    priority                = null
-    ranges                  = ["130.211.0.0/22","35.191.0.0/16"]
-    source_tags             = null
-    source_service_accounts = null
-    target_tags             = null
-    target_service_accounts = null
-    allow = [{
-      protocol = "tcp"
-      ports    = ["0-65535"]
-    }]
-    deny = []
-    log_config = {
-      metadata = "INCLUDE_ALL_METADATA"
-    }
-  }]
-}
-
-// Install gateway api crds on each cluster
-resource "null_resource" "exec_mcg_crds" {
-  for_each = var.cluster_config
+// Create Kubeconfig
+resource "null_resource" "create_kube_config" {
   provisioner "local-exec" {
     interpreter = ["bash", "-exc"]
-    command     = "scripts/install_crds.sh"
-    working_dir = path.module    
+    command     = "scripts/create_kube_config.sh"
+    working_dir = path.module
     environment = {
-      CLUSTER    = each.key
-      LOCATION   = each.value.region
+      CLUSTER    = var.cluster_name
+      LOCATION   = var.location
       PROJECT_ID    = var.project_id
-      GATEWAY_API_VERSION = var.gateway_crds_version
+      ASM_VERSION = var.asm_version
+      ASM_PACKAGE = var.asm_package
     }
   }
 }
 
-// enable Multi-cluster service discovery
-resource "google_gke_hub_feature" "mcs" {
-  name = "multiclusterservicediscovery"
-  location = "global"
-  project  = var.project_id
-  provider = google-beta
-}
-
-// enable Multi-cluster Ingress(also gateway) project wide
-resource "google_gke_hub_feature" "mci" {
+// Install asm crds on each cluster
+resource "null_resource" "install_mesh" {
   depends_on = [
-    null_resource.exec_mcg_crds,
+    null_resource.create_kube_config,
   ]
-  name = "multiclusteringress"
-  location = "global"
-  project  = var.project_id
-  spec {
-    multiclusteringress {
-      config_membership = "projects/${var.project_id}/locations/global/memberships/${keys(var.cluster_config)[0]}-membership"
+  provisioner "local-exec" {
+    interpreter = ["bash", "-exc"]
+    command     = "scripts/install_mesh.sh"
+    working_dir = path.module
+    environment = {
+      CLUSTER    = var.cluster_name
+      LOCATION   = var.location
+      PROJECT_ID    = var.project_id
     }
   }
-  provider = google-beta
 }
 
-// Create IAM binding allowing the hub project's GKE Hub service account access to the registered member project
-resource "google_project_iam_binding" "gkehub-serviceagent" {
-  role    = "roles/gkehub.serviceAgent"
-  project = var.project_id
-  members = [
-    "serviceAccount:service-${data.google_project.project.number}@gcp-sa-gkehub.iam.gserviceaccount.com",
-  ]
-}
-
-// Create IAM binding allowing the hub project's MCS service account access to the shared vpc project
-resource "google_project_iam_binding" "host-serviceagent" {
-  role    = "roles/multiclusterservicediscovery.serviceAgent"
-  project = var.vpc_project_id
-  members = [
-    "serviceAccount:service-${data.google_project.project.number}@gcp-sa-gkehub.iam.gserviceaccount.com",
-  ]
-}
-
-// Create IAM binding allowing the hub project's MCS service account access to the gke cluster project
-resource "google_project_iam_binding" "member-serviceagent" {
-  role    = "roles/multiclusterservicediscovery.serviceAgent"
-  project = var.project_id
-  members = [
-    "serviceAccount:service-${data.google_project.project.number}@gcp-sa-gkehub.iam.gserviceaccount.com",
-  ]
-}
-
-resource "google_project_iam_binding" "network-viewer-member" {
+// Install asm crds on each cluster
+resource "null_resource" "swap_secrets" {
   depends_on = [
-    resource.google_gke_hub_feature.mcs,
+    null_resource.install_mesh,
   ]
-  role    = "roles/compute.networkViewer"
-  project = var.project_id
-  members = [
-    "serviceAccount:${var.project_id}.svc.id.goog[gke-mcs/gke-mcs-importer]",
-  ]
+  for_each = local.cluster_count != 1 ? var.cluster_config : {}
+  provisioner "local-exec" {
+    interpreter = ["bash", "-exc"]
+    command     = "scripts/swap_secrets.sh"
+    working_dir = path.module
+    environment = {
+      CLUSTER    = var.cluster_name
+      LOCATION   = var.location
+      TARGET_CLUSTER    = each.key
+      TARGET_LOCATION   = each.value.region
+      PROJECT_ID    = var.project_id
+      ASM_VERSION = var.asm_version
+      ASM_PACKAGE = var.asm_package
+    }
+  }
 }
 
-resource "google_project_iam_binding" "container-admin-mcgsa" {
-  role    = "roles/container.admin"
-  project = var.project_id
-  depends_on = [
-    resource.google_gke_hub_feature.mci,
-  ]
-  members = [
-    "serviceAccount:service-${data.google_project.project.number}@gcp-sa-multiclusteringress.iam.gserviceaccount.com",
-  ]
-}
+# Safer cluster creates the rule below - leaving just in case we decide to change the cluster module and need to implement a FW rule to allow intra-cluster traffic
+# module "firewall_rules" {
+#   source       = "terraform-google-modules/network/google//modules/firewall-rules"
+#   project_id   = var.vpc_project_id
+#   network_name = var.vpc_name
+
+#   rules = [{
+#     name                    = "allow-all-10"
+#     description             = null
+#     direction               = "INGRESS"
+#     priority                = null
+#     ranges                  = ["10.0.0.0/8"]
+#     source_tags             = null
+#     source_service_accounts = null
+#     target_tags             = null
+#     target_service_accounts = null
+#     allow = [{
+#       protocol = "tcp"
+#       ports    = ["0-65535"]
+#     }]
+#     deny = []
+#     log_config = {
+#       metadata = "INCLUDE_ALL_METADATA"
+#     }
+#   }]
+# }
