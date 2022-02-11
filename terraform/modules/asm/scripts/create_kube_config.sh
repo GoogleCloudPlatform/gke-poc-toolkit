111b7f514cd32bc5cb1a08a28bba21c333b56882
Nicholas Eberts <nickeberts@google.com>
2022-02-11 09:22:59 -0500

    up

--- terraform/modules/asm/scripts/create_kube_config.sh
+++ terraform/modules/asm/scripts/create_kube_config.sh
@@ -4,23 +4,26 @@
 echo -e "Project is ${PROJECT_ID}"
 echo -e "CLUSTER is ${CLUSTER}"
 echo -e "LOCATION is ${LOCATION}"
+echo -e "ASM_PACKAGE is ${ASM_PACKAGE}"
 
 # Create kubeconfig and get cluster creds
 export WORKDIR=`pwd`
 echo -e "Adding cluster ${CLUSTER} to kubeconfig located at ${WORKDIR}/tempkubeconfig"
 if [[ ! -e ${WORKDIR}/tempkubeconfig ]]; then
+    echo -e "Creating tempkubeconfig."
     touch ${WORKDIR}/tempkubeconfig
 fi
+echo -e "tempkubeconfig already exists."
 export KUBECONFIG=${WORKDIR}/tempkubeconfig
 gcloud container clusters get-credentials ${CLUSTER} --region ${LOCATION} --project ${PROJECT_ID}
 
 # Download ASM installation package for istioctl bin
+if [[ ${OSTYPE} == 'darwin'* ]]; then
+    export ASM_PACKAGE_OS="${ASM_PACKAGE}-osx.tar.gz"
+else 
+    export ASM_PACKAGE_OS="${ASM_PACKAGE}-linux-amd64.tar.gz"
+fi
 if [[ ! -e ./${ASM_PACKAGE_OS}/bin/istioctl ]]; then
-    if [[ ${OSTYPE} == 'darwin'* ]]; then
-        export ASM_PACKAGE_OS="${ASM_PACKAGE}-osx.tar.gz"
-    else 
-        export ASM_PACKAGE_OS="${ASM_PACKAGE}-linux-amd64.tar.gz"
-    fi
     curl -LO https://storage.googleapis.com/gke-release/asm/"${ASM_PACKAGE_OS}"
     tar xzf ./${ASM_PACKAGE_OS} && rm -rf ./${ASM_PACKAGE_OS}
 fi
