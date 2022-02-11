5384db2bc970cfd05a8ffb22515403cd74bdc25c
Nicholas Eberts <nickeberts@google.com>
2022-02-11 14:56:19 -0500

    up

--- terraform/modules/asm/scripts/create_kube_config.sh
+++ terraform/modules/asm/scripts/create_kube_config.sh
@@ -18,12 +18,12 @@ export KUBECONFIG=${WORKDIR}/tempkubeconfig
 gcloud container clusters get-credentials ${CLUSTER} --region ${LOCATION} --project ${PROJECT_ID}
 
 # Download ASM installation package for istioctl bin
-if [[ ${OSTYPE} == 'darwin'* ]]; then
-    export ASM_PACKAGE_OS="${ASM_PACKAGE}-osx.tar.gz"
-else 
-    export ASM_PACKAGE_OS="${ASM_PACKAGE}-linux-amd64.tar.gz"
-fi
-if [[ ! -e ${ASM_PACKAGE_OS}/bin/istioctl ]]; then
+if [[ ! -e ${ASM_PACKAGE}/bin/istioctl ]]; then
+    if [[ ${OSTYPE} == 'darwin'* ]]; then
+        export ASM_PACKAGE_OS="${ASM_PACKAGE}-osx.tar.gz"
+    else 
+        export ASM_PACKAGE_OS="${ASM_PACKAGE}-linux-amd64.tar.gz"
+    fi
     curl -LO https://storage.googleapis.com/gke-release/asm/"${ASM_PACKAGE_OS}"
-    tar xzf ${ASM_PACKAGE_OS}
+    tar xzf ${ASM_PACKAGE}
 fi
