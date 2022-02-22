01f5fa1914853fa1c22d6130c5926ed6bdf6c318
Nicholas Eberts <nickeberts@google.com>
2022-02-22 14:27:00 -0500

    Moved asm enablement into kubeconfig script so it only runs once. This helps on reducing build time

--- terraform/modules/asm/scripts/create_kube_config.sh
+++ terraform/modules/asm/scripts/create_kube_config.sh
@@ -48,3 +48,29 @@ if [[ ${NUM_CONTEXTS} != ${NUM_CLUSTERS} ]]; then
 else
     echo -e "Kubeconfig is setup with all gketoolkit clusters credentials"
 fi
+
+# Enable ASM Mesh which installs ASM CRDs
+echo -e "Enabling ASM Mesh on the GKE HUB"
+gcloud beta container hub mesh enable --project=${PROJECT_ID}
+
+NUM_MEMBERS=`gcloud beta container hub memberships list --project=${PROJECT_ID} --format="value(name)" | wc -l | awk '{print $1}'`
+echo -e "NUM_MEMBERS: ${NUM_MEMBERS}"
+STATE_CODE="OK"
+if [[ ${NUM_MEMBERS} -le 1 ]]; then
+    echo -e "STATE_CODE: ${STATE_CODE}"
+else
+    for i in {2..${NUM_MEMBERS}}; do
+        STATE_CODE="${STATE_CODE};OK"
+    done
+    echo -e "STATE_CODE: ${STATE_CODE}"
+fi
+
+for i in {1..900}; do
+    STATUS=`gcloud beta container hub mesh describe --project=${PROJECT_ID} --format="value(membershipStates[].state.code)"`
+    if [[ ${STATUS} == ${STATE_CODE} ]]; then
+        echo -e "Hub mesh membership stats all OK"
+        break
+    else
+        echo -e "Waiting on hub to install ASM in clusters for $i seconds. Current status:${STATUS}. Expected status ${STATE_CODE}"
+    fi
+done
--- terraform/modules/asm/scripts/install_mesh.sh
+++ terraform/modules/asm/scripts/install_mesh.sh
@@ -11,32 +11,6 @@ export WORKDIR=`pwd`
 echo -e "Setting up kubeconfig at ${WORKDIR}/tempkubeconfig"
 export KUBECONFIG=${WORKDIR}/tempkubeconfig
 
-# Enable ASM Mesh which installs ASM CRDs
-echo -e "Enabling ASM Mesh on the GKE HUB"
-gcloud beta container hub mesh enable --project=${PROJECT_ID}
-
-NUM_MEMBERS=`gcloud beta container hub memberships list --project=${PROJECT_ID} --format="value(name)" | wc -l | awk '{print $1}'`
-echo -e "NUM_MEMBERS: ${NUM_MEMBERS}"
-STATE_CODE="OK"
-if [[ ${NUM_MEMBERS} -le 1 ]]; then
-    echo -e "STATE_CODE: ${STATE_CODE}"
-else
-    for i in {2..${NUM_MEMBERS}}; do
-        STATE_CODE="${STATE_CODE};OK"
-    done
-    echo -e "STATE_CODE: ${STATE_CODE}"
-fi
-
-for i in {1..900}; do
-    STATUS=`gcloud beta container hub mesh describe --project=${PROJECT_ID} --format="value(membershipStates[].state.code)"`
-    if [[ ${STATUS} == ${STATE_CODE} ]]; then
-        echo -e "Hub mesh membership stats all OK"
-        break
-    else
-        echo -e "Waiting on hub to install ASM in clusters for $i seconds. Current status:${STATUS}. Expected status ${STATE_CODE}"
-    fi
-done
-
 # Verify CRD is established in the cluster
 echo -e "Verifying Control Plane Revisions CRD is present on ${CLUSTER}"
 kubectl wait --for=condition=established crd controlplanerevisions.mesh.cloud.google.com --timeout=20m \
