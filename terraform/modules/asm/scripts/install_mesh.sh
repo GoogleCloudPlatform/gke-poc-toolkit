9a2b16b804ed99c1ecd406cf236acbe1409003fa
Nick Eberts <nickeberts@google.com>
2022-10-10 13:46:02 -0400

    added connet gw creds to scripts

--- terraform/modules/asm/scripts/install_mesh.sh
+++ terraform/modules/asm/scripts/install_mesh.sh
@@ -15,18 +15,19 @@ touch ${WORKDIR}/${kubeconfig}
 export KUBECONFIG=${WORKDIR}/${kubeconfig}
 
 # Get cluster creds
-gcloud container clusters get-credentials ${CLUSTER} --region ${LOCATION} --project ${PROJECT_ID}
+gcloud beta container fleet memberships get-credentials ${CLUSTER}-membership --project ${PROJECT_ID}
+#gcloud container clusters get-credentials ${CLUSTER} --region ${LOCATION} --project ${PROJECT_ID}
 
 # Verify CRD is established in the cluster
 echo -e "Verifying Control Plane Revisions CRD is present on ${CLUSTER}"
-until kubectl get mutatingwebhookconfigurations istiod-asm-managed --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+until kubectl get mutatingwebhookconfigurations istiod-asm-managed --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
   do
     echo -n "...still waiting for ASM MCP webhook creation"
     sleep 1
   done
 
 # Install SAs, Roles, Roledbinding for ASM
-kubectl apply -f ./manifests/istio-system-ns.yaml --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
-kubectl apply -f ./manifests/ --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+kubectl apply -f ./manifests/istio-system-ns.yaml --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+kubectl apply -f ./manifests/ --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
 
-kubectl wait --for=condition=ProvisioningFinished controlplanerevision -n istio-system asm-managed --timeout=10m --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+kubectl wait --for=condition=ProvisioningFinished controlplanerevision -n istio-system asm-managed --timeout=10m --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
--- terraform/modules/mcg/scripts/install_crds.sh
+++ terraform/modules/mcg/scripts/install_crds.sh
@@ -15,22 +15,24 @@ touch ${WORKDIR}/${kubeconfig}
 export KUBECONFIG=${WORKDIR}/${kubeconfig}
 
 # Get cluster creds
-gcloud container clusters get-credentials ${CLUSTER} --region ${LOCATION} --project ${PROJECT_ID}
+gcloud beta container fleet memberships get-credentials ${CLUSTER}-membership --project ${PROJECT_ID}
+
+#gcloud container clusters get-credentials ${CLUSTER} --region ${LOCATION} --project ${PROJECT_ID}
 
 # Install Gateway API CRDs
 echo -e "Installing GatewayAPI CRDs"
 
-kubectl apply -k "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.4.2" --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+kubectl apply -k "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.4.2" --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
 
-kubectl apply -k "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.3.0" --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+kubectl apply -k "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.3.0" --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
 
 # Verify CRD is established in the cluster
 echo -e "Validating GatewayAPI CRD creation"
-kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/backendpolicies.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
-kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/gatewayclasses.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
-kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/httproutes.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
-kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/gateways.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
-kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/tcproutes.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
-kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/tlsroutes.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
-kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/udproutes.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=gke_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/backendpolicies.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/gatewayclasses.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/httproutes.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/gateways.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/tcproutes.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/tlsroutes.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
+kubectl wait --for=condition=established customresourcedefinition.apiextensions.k8s.io/udproutes.networking.x-k8s.io --timeout=10m --kubeconfig ${KUBECONFIG} --context=connectgateway_${PROJECT_ID}_${LOCATION}_${CLUSTER}
 
